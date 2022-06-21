b27b624e90ce9380a03873ca47816ab61a6cd4e8
ad1mohan <adi.mohan1999@gmail.com>
2022-06-21 18:50:58 +0530

    Materials, Lights, environment maps

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ import gsap from 'gsap'
 import * as dat from 'dat.gui'
 import { TextureLoader } from 'three'
 
-// Testures
+// Materials, Lights, environment maps
 const loadingManager = new THREE.LoadingManager()
 loadingManager.onStart =()=>{
     console.log('onStart')
@@ -20,23 +20,37 @@ loadingManager.onError =()=>{
     console.log('onError')
 }
 const textureLoader = new THREE.TextureLoader(loadingManager);
-const colorTexture = textureLoader.load('/textures/door/color.jpg')
-const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-const heightTexture = textureLoader.load('/textures/door/height.jpg')
-const normalTexture = textureLoader.load('/textures/door/normal.jpg')
-const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
-const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const cubeTextureLoader = new THREE.CubeTextureLoader();
 
-colorTexture.repeat.x = 2
-colorTexture.repeat.y = 3
-colorTexture.wrapS = THREE.RepeatWrapping
-colorTexture.wrapT = THREE.RepeatWrapping
-colorTexture.offset.x = 0.5
-colorTexture.offset.y = 0.5
-colorTexture.rotation= Math.PI*0.25
-colorTexture.center.x=0.5
-colorTexture.center.y=0.5
+const colorDoorTexture = textureLoader.load('/textures/door/color.jpg')
+const alphaDoorTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightDoorTexture = textureLoader.load('/textures/door/height.jpg')
+const normalDoorTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionDoorTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const metalnessDoorTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessDoorTexture = textureLoader.load('/textures/door/roughness.jpg')
+
+const matcap1Texture = textureLoader.load('/textures/matcaps/7.png')
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+// colorTexture.wrapS = THREE.RepeatWrapping
+// colorTexture.wrapT = THREE.RepeatWrapping
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+// colorTexture.rotation= Math.PI*0.25
+// colorTexture.center.x=0.5
+// colorTexture.center.y=0.5
+
+const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/1/px.jpg',
+    '/textures/environmentMaps/1/nx.jpg',
+    '/textures/environmentMaps/1/py.jpg',
+    '/textures/environmentMaps/1/ny.jpg',
+    '/textures/environmentMaps/1/pz.jpg',
+    '/textures/environmentMaps/1/nz.jpg',
+])
 
 const sizes = {
     width:window.innerWidth, 
@@ -60,11 +74,66 @@ const canvas = document.querySelector('.webgl-canvas')
 // Scene
 const scene = new THREE.Scene()
 
-// const mat = new THREE.MeshBasicMaterial({color:parameters.color, wireframe:false})
-const mat = new THREE.MeshBasicMaterial({map:colorTexture})
-const geo = new THREE.BoxGeometry(1,1,1)
-const mesh = new THREE.Mesh(geo,mat)
-scene.add(mesh);
+// const mat = new THREE.MeshBasicMaterial({map:colorTexture})
+// const mat = new THREE.MeshBasicMaterial()
+// mat.map = colorDoorTexture
+// mat.transparent=true
+// mat.alphaMap = alphaDoorTexture
+// mat.side = THREE.DoubleSide
+
+// const mat = new THREE.MeshNormalMaterial()
+// mat.flatShading = true
+
+// const mat = new THREE.MeshMatcapMaterial()
+// mat.matcap = matcap1Texture
+
+// const mat = new THREE.MeshDepthMaterial()
+
+// const mat = new THREE.MeshLambertMaterial()
+
+// const mat = new THREE.MeshPhongMaterial()
+// mat.shininess = 150
+// mat.specular = new THREE.Color(0x1232ff)
+
+// const mat = new THREE.MeshToonMaterial()
+// mat.gradientMap = gradientTexture
+// gradientTexture.minFilter = THREE.NearestFilter
+// gradientTexture.magFilter = THREE.NearestFilter
+
+const mat = new THREE.MeshStandardMaterial()
+mat.metalness = 0.45
+mat.roughness = 0.45
+mat.envMap=environmentMapTexture
+
+// const mat = new THREE.MeshStandardMaterial()
+// mat.metalness = 0.45
+// mat.roughness = 0.45
+// mat.transparent = true
+// mat.map = colorDoorTexture
+// mat.aoMap = ambientOcclusionDoorTexture
+// mat.displacementMap = heightDoorTexture
+// mat.displacementScale = 0.05
+// mat.metalnessMap = metalnessDoorTexture
+// mat.roughnessMap = roughnessDoorTexture
+// mat.normalMap = normalDoorTexture
+// mat.alphaMap = alphaDoorTexture
+
+const sphere = new THREE.Mesh(new THREE.SphereBufferGeometry(0.5,64,64),mat)
+const plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(1,1,100,100),mat)
+plane.geometry.setAttribute('uv2',new THREE.BufferAttribute(plane.geometry.attributes.uv.array,2))
+const torus = new THREE.Mesh(new THREE.TorusBufferGeometry(0.3,0.2,64,128),mat)
+scene.add(sphere,plane, torus);
+
+sphere.position.x = - 2
+torus.position.x = 2
+
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff,0.5)
+const pointLight = new THREE.PointLight(0xffffff,0.5)
+pointLight.x = 2
+pointLight.y = 3
+pointLight.z = 4
+scene.add(ambientLight, pointLight)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height)
@@ -119,13 +188,17 @@ window.addEventListener('dblclick',()=>{
 })
 
 // gui tweeks
-gui.add(mesh.position, 'x' ) // brings text
-gui.add(mesh.position, 'y' ,-3,3,0.01) // brings range
-gui.add(mesh.position, 'z').min(-3).max(3).step(0.01).name('zoom')  // brings range with name
-gui.add(mesh,'visible').name("Mesh Visible") // boolean/checkbox
-gui.add(axesHelper,'visible').name("Axes Helper Visible") // boolean/checkbox
-gui.addColor(parameters,'color').onChange(()=>{mat.color.set(parameters.color)}) // colour
-gui.add(parameters,'spin')
+// gui.add(sphere.position, 'x' ) // brings text
+// gui.add(sphere.position, 'y' ,-3,3,0.01) // brings range
+// gui.add(sphere.position, 'z').min(-3).max(3).step(0.01).name('zoom')  // brings range with name
+// gui.add(sphere,'visible').name("Mesh Visible") // boolean/checkbox
+// gui.add(axesHelper,'visible').name("Axes Helper Visible") // boolean/checkbox
+// gui.addColor(parameters,'color').onChange(()=>{mat.color.set(parameters.color)}) // colour
+// gui.add(parameters,'spin')
+gui.add(mat ,'metalness').min(0).max(1).step(0.01).name('metalness')
+gui.add(mat ,'roughness').min(0).max(1).step(0.01).name('roughness')
+gui.add(mat ,'aoMapIntensity').min(0).max(5).step(0.01).name('aoMapIntensity')
+gui.add(mat ,'displacementScale').min(0).max(5).step(0.01).name('displacementScale')
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -135,8 +208,15 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width,sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
 
+const clock = new THREE.Clock()
 const tick = ()=>{
-
+    const elaspedTime = clock.getElapsedTime()
+    sphere.rotation.x = 0.1 * elaspedTime
+    plane.rotation.x = 0.1 * elaspedTime
+    torus.rotation.x = 0.1 * elaspedTime
+    sphere.rotation.y = 0.15  * elaspedTime
+    plane.rotation.y = 0.15  * elaspedTime
+    torus.rotation.y = 0.15  * elaspedTime
     controls.update()
     renderer.render(scene,camera)
     window.requestAnimationFrame(tick)
